2cc0aa08b398d8a73d60891e37e2d809c544fb66
ManishBangari <manishbangari2108@gmail.com>
2024-07-15 13:34:44 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.dsproject.logger import logging
 from src.dsproject.exception import CustomException
 from src.dsproject.components.data_ingestion  import DataIngestion, DataIngestionConfig
 from src.dsproject.components.data_transformation  import DataTransformationConfig,DataTransformation
-import sys
+from src.dsproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 if __name__=="__main__":
     logging.info("Execution has started.")
@@ -15,7 +15,11 @@ if __name__=="__main__":
 
         #data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+        train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+
+        # Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
     except Exception as e:
         logging.info("Custom Exception")
