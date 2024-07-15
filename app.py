2c39a792e2939b02cb6eb006dd8cbb52ca55d7c1
ManishBangari <manishbangari2108@gmail.com>
2024-07-15 13:05:36 +0530

    data transformation

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import sys
 from src.dsproject.logger import logging
 from src.dsproject.exception import CustomException
-from src.dsproject.components.data_ingestion  import DataIngestion
+from src.dsproject.components.data_ingestion  import DataIngestion, DataIngestionConfig
+from src.dsproject.components.data_transformation  import DataTransformationConfig,DataTransformation
+import sys
 
 if __name__=="__main__":
     logging.info("Execution has started.")
@@ -9,7 +11,11 @@ if __name__=="__main__":
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config = DataTransformationConfig()
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
     except Exception as e:
         logging.info("Custom Exception")
